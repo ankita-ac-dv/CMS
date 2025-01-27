33c49ba888e9074a1504eb97fecdb612e87b8cf4
Ankita Kumari <ankita.ac.dv@gmail.com>
2025-01-27 22:01:32 +0530

    initial commit

--- connect.php
+++ connect.php
@@ -1,3 +1,13 @@
 <?php
-
+   $host="localhost";
+   $user="root";
+   $password="";
+   $database="priya";
+   $connect=mysqli_connect($host,$user,$password,$database);
+   if (mysqli_connect_error()) {
+    die("database not connect".mysqli_connect_error);
+   }
+   else{
+    echo("database successful connect");
+   }
 ?>
