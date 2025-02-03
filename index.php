9ac2a460c399fed876299badcef83c3484c14582
Ankita Kumari <ankita.ac.dv@gmail.com>
2025-02-03 22:29:05 +0530

    inital commit

--- index.php
+++ index.php
@@ -26,11 +26,6 @@
         <p>This is a description for the second card. Add some details here.</p>
         <a href="Manager_Login.php"><button>Login</button></a>
     </div>
-    <div class="card">
-        <h3>Manager Login</h3>
-        <p>This is a description for the second card. Add some details here.</p>
-        <a href="Manager_Login.php"><button>Login</button></a>
-    </div>
 </div>
 
 </body>
