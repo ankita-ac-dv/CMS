0a445a8c836a3ad071f071814b9e3014d596f220
Ankita Kumari <ankita.ac.dv@gmail.com>
2025-02-24 17:04:23 +0530

    initl commit

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
         <p>This is a description for the second card. Add some details here.</p>
         <a href="Manager_Login.php"><button>Login</button></a>
     </div>
-    <div class="card">
-        <h3>Manager Logi</h3>
+    <div class="car">
+        <h3>Manager Login</h3>
         <p>This is a description for the second card. Add some details here.</p>
         <a href="Manager_Login.php"><button>Login</button></a>
 </div>
